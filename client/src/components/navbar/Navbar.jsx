27ae4cf88f0620e4ea846be53bdd3f6ee223652b
Jason Pallone <50902289+Jason-Pallone@users.noreply.github.com>
2022-05-06 17:43:35 -0400

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo image" />
       </div>
       <ul className='app__navbar-links'>
-        {[, "About", "Contact", "Work", "Skills", "Testimonials"].map((items) => (
+        {["About", "Contact", "Work", "Skills", "Testimonials"].map((items) => (
           <li key={`link-${items}`} className='app__flex p-text'>
             <div />
             <a href={`#${items}`}>{`${items}`}</a>
